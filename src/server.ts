fedbaf46d54167994899f5bed5f5c609cacbb632
edsondearaujo <edsondearaujofga@gmail.com>
2020-10-20 19:04:11 -0300

    feat(#4): Data_base table UP

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-import './database/connection';
+//import './database/';
 
 const app = express();
 
 app.use(express.json());    
 
-app.get('/users', (request, response)=> {
+app.get('/users/:id', (request, response)=> {
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
+
+
     return response.json({ message: 'teste json' });
 });
 
